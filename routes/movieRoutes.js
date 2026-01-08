62e16e8d0d44fa4531c76d52675ce97d274560a9
KaeTheDev <kaethedev@gmail.com>
2026-01-08 00:16:23 -0500

    Added movie details to movieRoutes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,6 +2,10 @@ const express  = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
 
+// Search Movie Results
 router.get('/search', movieController.searchMovies);
+
+// Get Movie Details
+router.get('/:id', movieController.getMovieDetails);
 
 module.exports = router;
