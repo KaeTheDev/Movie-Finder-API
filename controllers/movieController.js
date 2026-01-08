da236a9e1e3f1b25bb61dbd9a2c9cdca5a117d59
KaeTheDev <kaethedev@gmail.com>
2026-01-08 00:15:21 -0500

    Add getMovieDetails to movieController

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -32,4 +32,35 @@ const searchMovies = async(req, res) => {
     }
 };
 
-module.exports = { searchMovies };
+// SEARCH MOVIE DETAILS
+const getMovieDetails = async(req, res) => {
+    const { id } = req.query;
+
+    // 1. Validation
+    if(!id) {
+        return res.status(400).json({
+            error: 'Id query parameter is required'
+        });
+    }
+
+    try {
+        // Call OMDb API
+        const response = await axios.get('https://www.omdbapi.com/', {
+            params: {
+                i: id,
+                apikey: process.env.OMDB_API_KEY, 
+            },
+        });
+
+        // send response back to client
+        res.json(response.data);
+    } catch(error) {
+        console.error('OMDb API error:', error.response?.data || error.message);
+
+        res.status(500).json({
+            error: 'Failed to fetch movie details'
+        });
+    }
+};
+
+module.exports = { searchMovies, getMovieDetails };
